524bbaa7596028510b3af495568e5bb0a1573bb3
abhinav <abhinav200218@gmail.com>
2023-11-10 02:33:25 +0530

    first

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,15 +1,35 @@
 import './dashboard.css'
+import Charging from '../../contracts/evcharging.json'
+import getMetamask from "@/lib/getMetamask";
+const abi = Charging.abi;
+
 
 export default function Dash(){
+    const contractAddress = "0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8"
 
-    return(
-       <div>
-            <div className="title">
-              helllo shabnam
-            </div>
-            
-       
-       </div>
-     
-    )
+    const transact= async()=>{
+     const metamask = await getMetamask();
+     const signer= await metamask.getSigner(0);
+     const contract = new ethers.Contract(contractAddress, abi, signer);
+     const contractwithsigner = contract.connect(signer);
+     const transaction = await contractwithsigner.transfer();
+     await transaction.wait();
+     console.log(transaction)
+
+    }
+
+    return(<>
+    <div id="charge">
+        <h1>Current Charge/Unit Price: $0.10</h1>
+    </div>
+
+    <div id="credits">
+        <h2>Credits: 5.00</h2>
+    </div>
+
+    <div id="payment">
+        <button id="proceedToPayment">Proceed to Payment</button>
+    </div>
+    
+    </>)
 }
--- app/page.js
+++ app/page.js
@@ -1,15 +1,37 @@
-
-import './page.css';
+"use client";
+import "./page.css";
+import {ethers} from "ethers";
 
 export default function Home() {
-    return (<>
-        <div id="charge">
-            Current Charge/Unit Price: $0.10
-        </div>
+    const getMetamask = async () => {
+        if (typeof window !== "undefined") {
+            const {ethereum} = window;
+            if (!ethereum) {
+                alert("Please install Metamask");
+            }
+            const accounts = await ethereum.request({
+                method: "eth_requestAccounts",
+            });
+            const provider = new ethers.providers.Web3Provider(ethereum, "sepola");
+            const signer = provider.getSigner(0);
+            localStorage.setItem("account", accounts[0]);
+            console.log(accounts[0]);
+        }
+    };
+    return(
+        <>
+            <div id="charge">
+                Current Charge/Unit Price: $0.10
+            </div>
 
-        <div id="calculator">
-            <input type="text" id="accountNumber" placeholder="Enter Account Number"/>
-                <button id="enterButton">Enter</button>
-        </div>
-    </>)
+            <div id="calculator">
+                <button id="enterButton" onClick={getMetamask}>Connect to Metamask</button>
+                <br />
+                <input type="text" id="accountNumber" placeholder="Enter Account Number"/>
+                <br />
+                    <button id="enterButton">Enter</button>
+            </div>
+        </>
+    )
+
 }
