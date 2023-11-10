adef19a815a25cc08c9698dd2db3fc88da563c03
abhinav <abhinav200218@gmail.com>
2023-11-10 21:12:20 +0530

    last

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,35 +1,67 @@
+"use client"
 import './dashboard.css'
+import {ethers} from "ethers"
 import Charging from '../../contracts/evcharging.json'
 import getMetamask from "@/lib/getMetamask";
-const abi = Charging.abi;
+const Abi = Charging.output.abi;
 
 
 export default function Dash(){
-    const contractAddress = "0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8"
+    const contractAddress = '0xd9145CCE52D386f254917e481eB44e9943F39138';
 
     const transact= async()=>{
-     const metamask = await getMetamask();
-     const signer= await metamask.getSigner(0);
-     const contract = new ethers.Contract(contractAddress, abi, signer);
+      await console.log(Charging);
+     const metamask = await new ethers.BrowserProvider(ethereum,"any");  
+     const signer= await metamask.getSigner();
+     const contract =await new ethers.Contract(contractAddress, Abi, signer);
      const contractwithsigner = contract.connect(signer);
-     const transaction = await contractwithsigner.transfer();
+     const transaction = await contractwithsigner.transfer('0xC4C7AACE8A168B7DCdD0dD0bded0D1F329aaD1dc',5);
      await transaction.wait();
      console.log(transaction)
 
     }
 
     return(<>
-    <div id="charge">
-        <h1>Current Charge/Unit Price: $0.10</h1>
-    </div>
+ 
+<div id="full">
+  <div id="content-box">
+    <nav id="navbar">
+      <div id="left-link">Current Charge/Unit Price</div>
+      <div id="right-link">Wallet Balance</div>
+    </nav>
 
-    <div id="credits">
-        <h2>Credits: 5.00</h2>
-    </div>
+    <div id="container">
+      <div id="charge-info">
+        <div id="current-charge">
+          Current Charge: Rs. <span id="charge-value">50</span>
+        </div>
+      </div>
 
-    <div id="payment">
-        <button id="proceedToPayment">Proceed to Payment</button>
+      <div id="wallet">
+        <div id="wallet-balance">
+          Wallet Balance: Rs. <span id="wallet-value">100</span> <img src="wallet_icon.png" alt="Wallet Icon"/>
+        </div>
+      </div>
+
+      <div id="combined-box">
+        <div id="credits-earned">
+          Credits Earned: Rs. <span id="credits-value">5</span>
+        </div>
+
+        <div id="charger-status">
+          Charger Status: Available 
+        </div>
+
+        <div id="proceed-to-payment">
+          <button onClick={transact}>Proceed to Payment</button>
+        </div>
+      </div>
     </div>
+  </div>
+  </div>
+
+
+
     
-    </>)
+    </>);
 }
--- app/page.js
+++ app/page.js
@@ -1,37 +1,52 @@
 "use client";
 import "./page.css";
-import {ethers} from "ethers";
+import { ethers } from "ethers";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-    const getMetamask = async () => {
-        if (typeof window !== "undefined") {
-            const {ethereum} = window;
-            if (!ethereum) {
-                alert("Please install Metamask");
-            }
-            const accounts = await ethereum.request({
-                method: "eth_requestAccounts",
-            });
-            const provider = new ethers.providers.Web3Provider(ethereum, "sepola");
-            const signer = provider.getSigner(0);
-            localStorage.setItem("account", accounts[0]);
-            console.log(accounts[0]);
-        }
-    };
-    return(
-        <>
-            <div id="charge">
-                Current Charge/Unit Price: $0.10
-            </div>
-
-            <div id="calculator">
-                <button id="enterButton" onClick={getMetamask}>Connect to Metamask</button>
-                <br />
-                <input type="text" id="accountNumber" placeholder="Enter Account Number"/>
-                <br />
-                    <button id="enterButton">Enter</button>
-            </div>
-        </>
-    )
-
+  const router = useRouter();
+  const getMetamask = async () => {
+    if (typeof window !== "undefined") {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Please install Metamask");
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const provider = new ethers.BrowserProvider(ethereum, "sepolia");
+      const signer = provider.getSigner(0);
+      localStorage.setItem("account", accounts[0]);
+      console.log(accounts[0]);
+      router.push("/dashboard");
+    }
+  };
+  return (
+    <>
+      <div id="full">
+        <nav id="navbar">
+          <div id="left-link"> Current Charge / Unit Price: 6.4 Rs </div>{" "}
+          <div id="right-link"> Wallet Balance: 10 ETH </div>{" "}
+        </nav>{" "}
+        <div id="container">
+          <div id="header"> EV Charging Portal </div>{" "}
+          <div id="welcome"> Welcome to EV Charging Portal </div>{" "}
+          <div id="map-container">
+            <img
+              src="https://www.researchgate.net/publication/347799272/figure/fig4/AS:1024435968565249@1621255976777/Location-of-charging-stations-in-Australia-derived-from-57_Q320.jpg"
+              alt="Map"
+            />
+          </div>{" "}
+          <div id="account-input">
+            <label for="account-number"> Account Number: </label>{" "}
+            <input type="text" id="account-number" name="account-number" />
+          </div>{" "}
+          <div id="submit-button">
+            <input type="submit" onClick={getMetamask} value="Enter" />
+          </div>{" "}
+        </div>{" "}
+      </div>{" "}
+    </>
+  );
 }
